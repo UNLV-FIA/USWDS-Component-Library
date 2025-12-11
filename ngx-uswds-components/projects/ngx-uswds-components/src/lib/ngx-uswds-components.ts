4ac8b9a72b3a6557e7503ad7464a420bea2fe2d6
Christopher Rosado <chris.rosado@unlv.edu>
2025-12-11 08:24:19 -0800

    initialize library

---
+++ ngx-uswds-components/projects/ngx-uswds-components/src/lib/ngx-uswds-components.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngx-uswds-components',
+  imports: [],
+  template: `
+    <p>
+      ngx-uswds-components works!
+    </p>
+  `,
+  styles: ``,
+})
+export class NgxUswdsComponents {
+
+}
---
+++ ngx-uswds-components/projects/ngx-uswds-components/src/public-api.ts
@@ -0,0 +1,5 @@
+/*
+ * Public API Surface of ngx-uswds-components
+ */
+
+export * from './lib/ngx-uswds-components';

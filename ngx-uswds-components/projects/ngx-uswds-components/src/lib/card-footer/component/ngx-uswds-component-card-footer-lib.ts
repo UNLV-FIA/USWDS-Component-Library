c20188821446f8cd5860d334ff277036706b27e8
Christopher Rosado <chris.rosado@unlv.edu>
2025-12-12 09:36:11 -0800

    integrate library into demo application. Add previously made components to the library. Documentation is next

---
+++ ngx-uswds-components/projects/ngx-uswds-components/src/lib/card-footer/component/ngx-uswds-component-card-footer-lib.ts
@@ -0,0 +1,9 @@
+import { Component} from '@angular/core';
+
+@Component({
+  selector: 'uswds-card-footer',
+  templateUrl: './ngx-uswds-component-card-footer-lib.html',
+})
+export class USWDSCardFooter {
+
+}
--- ngx-uswds-components/projects/uswds-component-demo/src/app/app.ts
+++ ngx-uswds-components/projects/uswds-component-demo/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { USWDSCard } from 'ngx-uswds-components';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [USWDSCard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

82e2977df964c96253c79fac6995c0fdfbfa79e1
Kelvyn Meyers <Kelvyn.Meyers@usda.gov>
2025-12-12 12:54:47 -0800

    Utilized USWDS-Button in demo application

--- ngx-uswds-workspace/projects/ngx-uswds-demo/src/app/app.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-demo/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UswdsButton } from 'ngx-uswds-lib';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, UswdsButton],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })

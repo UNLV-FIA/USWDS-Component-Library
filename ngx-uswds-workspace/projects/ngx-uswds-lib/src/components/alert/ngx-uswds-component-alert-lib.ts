ede52cfed3c85e312a92e2855ae4cd524c329293
Cedric Men <52368680+CMCedricM@users.noreply.github.com>
2026-01-12 16:22:55 -0800

    added switch statements for the icons

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/alert/ngx-uswds-component-alert-lib.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/alert/ngx-uswds-component-alert-lib.ts
@@ -2,7 +2,7 @@ import { Component, input, computed } from '@angular/core';
 import { CommonModule, NgClass, NgIf } from '@angular/common';
 
 type AlertType = 'Informative' | 'Warning' | 'Success' | 'Error' | 'Emergency'
-
+const assetsPath = '/assets/img'
 @Component({
   selector: 'lib-ngx-uswds-component-alert-lib',
   imports: [NgClass],
@@ -13,6 +13,8 @@ export class UswdsAlert {
   type = input.required<AlertType>();
   headerText = input('');
   text = input(''); 
+
+
 
   // Computed Variables to help with css later
   alertType = computed(() => {
@@ -28,6 +30,23 @@ export class UswdsAlert {
     'usa-alert--error': this.alertType() == 'Error',
     'usa-alert--emergency': this.alertType() == 'Emergency'
   }))
+
+  iconHref = computed(() => {
+    switch (this.type()) {
+      case 'Informative':
+        return `${assetsPath}/info.svg`
+      case 'Warning':
+        return `${assetsPath}/warning.svg`
+      case 'Success':
+        return `${assetsPath}/check_circle.svg`
+      case 'Error':
+        return `${assetsPath}/error.svg`
+      case 'Emergency':
+        return `${assetsPath}/error_outline.svg`
+      default:
+         return `${assetsPath}/info.svg`
+    }
+  })
  
 
 }

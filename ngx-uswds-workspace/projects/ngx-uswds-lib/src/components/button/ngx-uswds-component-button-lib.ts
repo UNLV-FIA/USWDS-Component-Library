2a87de4357f4714e5a187c7a96d68aa2c2d3be4d
Cedric Men <cedric.men@unlv.edu>
2026-01-28 15:54:44 -0800

    initial commit for the button

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/button/ngx-uswds-component-button-lib.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/button/ngx-uswds-component-button-lib.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-uswds-button',
@@ -6,6 +6,10 @@ import { Component, Input } from '@angular/core';
   templateUrl: './ngx-uswds-component-button-lib.html',
   styleUrl: './ngx-uswds-component-button-lib.css',
 })
-export class UswdsButton {
-  @Input() text: string = "EMPTY";
+export class UswdsButton implements OnInit{
+  text = input.required<string>(); 
+
+  ngOnInit(): void {
+    if(!this.text() || this.text().trim() == '') throw new Error("prop 'text' must be defined and cannot be empty string")
+  }
 }

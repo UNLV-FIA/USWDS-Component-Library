521e6c38da6552ae4a4b1dabce6cb671192dd1ed
Rosadc1 <153774743+Rosadc1@users.noreply.github.com>
2026-02-05 08:55:51 -0800

    Test git in vscode

---
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-test-2/uswds-test-2.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-uswds-test-2',
+  imports: [],
+  templateUrl: './uswds-test-2.html',
+  styleUrl: './uswds-test-2.scss',
+})
+export class UswdsTest2 {}

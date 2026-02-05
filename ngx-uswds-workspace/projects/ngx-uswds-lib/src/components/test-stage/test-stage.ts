5c2883efdc56f02db1be07b0281883fb0862f8b1
Rosadc1 <153774743+Rosadc1@users.noreply.github.com>
2026-02-04 16:14:50 -0800

    test

---
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/test-stage/test-stage.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-test-stage',
+  imports: [],
+  templateUrl: './test-stage.html',
+  styleUrl: './test-stage.scss',
+})
+export class TestStage {}

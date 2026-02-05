e1b6cf72a3637486830941e9107272533fecc92a
Rosadc1 <153774743+Rosadc1@users.noreply.github.com>
2026-02-04 16:16:36 -0800

    remove test component

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/test-stage/test-stage.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-test-stage',
-  imports: [],
-  templateUrl: './test-stage.html',
-  styleUrl: './test-stage.scss',
-})
-export class TestStage {}

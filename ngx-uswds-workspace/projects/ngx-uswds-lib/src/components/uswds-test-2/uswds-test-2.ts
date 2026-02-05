71a6fac4020ca2c0056bcce441ea0ec93dcfa72a
Rosadc1 <153774743+Rosadc1@users.noreply.github.com>
2026-02-05 08:56:58 -0800

    remove test files

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-test-2/uswds-test-2.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-uswds-test-2',
-  imports: [],
-  templateUrl: './uswds-test-2.html',
-  styleUrl: './uswds-test-2.scss',
-})
-export class UswdsTest2 {}

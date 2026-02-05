bfdc7ec0dfc587f900400a9e94127e8c31efa25d
Rosadc1 <153774743+Rosadc1@users.noreply.github.com>
2026-02-05 09:32:53 -0800

    commit for viewing github desktop error

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/public-api.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/public-api.ts
@@ -10,4 +10,5 @@ export * from './components/uswds-alert/uswds-alert'
 
 // Banner Component
 export * from './components/uswds-banner/uswds-banner';
+export * from './components/uswds-banner/uswds-banner';
 

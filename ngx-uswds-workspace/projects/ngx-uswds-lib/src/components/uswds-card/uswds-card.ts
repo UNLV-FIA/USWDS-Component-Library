42130cf8da07f38ded707d21d80c75120f2b12c8
Cedric Men <52368680+CMCedricM@users.noreply.github.com>
2026-02-25 11:50:05 -0800

    added inset and fixed code

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-card/uswds-card.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-card/uswds-card.ts
@@ -1,16 +1,14 @@
 import { Component, input } from '@angular/core';
 import { CardType, MediaCardFormat } from './card.types';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 
 @Component({
   selector: 'ngx-uswds-card',
-  imports: [],
+  imports: [CommonModule, NgTemplateOutlet],
   templateUrl: './uswds-card.html',
   styleUrl: './uswds-card.scss',
 })
 export class UswdsCard {
   cardType = input<CardType>('NonMedia');
   mediaCardType = input<MediaCardFormat>('None');
-  headerText = input<string>('');
-  mediaItem = input<string>('');
-  mediaText = input<string>('');
 }

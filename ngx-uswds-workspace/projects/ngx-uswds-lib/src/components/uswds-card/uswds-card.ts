3f71ece12eac62a92c9f830e12973dcb1c0b4162
Cedric Men <cedric.men@unlv.edu>
2026-03-06 16:35:44 -0800

    fixed list issue

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-card/uswds-card.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-card/uswds-card.ts
@@ -4,9 +4,12 @@ import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { UswdsCardGroup } from '../uswds-card-group/uswds-card-group';
 
 @Component({
-  selector: 'ngx-uswds-card',
+  selector: 'ngx-uswds-card, li[ngx-uswds-card]',
   imports: [CommonModule, NgTemplateOutlet],
   templateUrl: './uswds-card.html',
+  host: {
+    '[class]': 'hostClasses()',
+  },
   styleUrl: './uswds-card.scss',
 })
 export class UswdsCard {
@@ -15,5 +18,25 @@ export class UswdsCard {
   gridFormat = input<GridFormats>(['tablet-lg:grid-col-6', 'widescreen:grid-col-4']);
   asAList = computed(() => {
     return !!this.cardGroup;
+  });
+  hostClasses = computed(() => {
+    const classes: string[] = ['usa-card'];
+
+    switch (this.mediaCardType()) {
+      case 'FlagDefault':
+        classes.push('usa-card--flag', 'flex-1');
+        break;
+      case 'FlagRightInset':
+        classes.push('usa-card--flag', 'flex-1', 'usa-card--media-right');
+        break;
+      case 'MediaWHeader':
+        classes.push('usa-card--header-first');
+        break;
+    }
+
+    for (const gridClass of this.gridFormat()) {
+      classes.push(gridClass);
+    }
+    return classes.join(' ');
   });
 }

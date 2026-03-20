bcf1615fca8bc5aceb0fa23d2ccd307a640fb4a7
Cedric Men <cedric.men@unlv.edu>
2026-03-20 14:12:44 -0700

    updated docs for uswds-card-group

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-card-group/uswds-card-group.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-card-group/uswds-card-group.ts
@@ -8,133 +8,93 @@ import { CommonModule } from '@angular/common';
  * @selector  ngx-uswds-card-group
  * @example
   <ngx-uswds-card-group>
-      <li ngx-uswds-card mediaCardType="None" [gridFormat]="['tablet:grid-col-12', 'mobile-lg:grid-col-6']">
-        <span card-header>Card</span>
-        <span card-body>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate, eaque qui
-            officia recusandae.
-          </p>
-        </span>
-        <span card-footer>
-          <ngx-uswds-button type="button">
-            <span>Visit Florida Keys</span>
-          </ngx-uswds-button>
-        </span>
-      </li>
+    <ngx-uswds-card mediaCardType="None" [gridFormat]="['tablet:grid-col-12']">
+      <span card-header>Standard Card</span>
+      <span card-body>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          possimus similique nemo odit doloremque laudantium?
+        </p>
+      </span>
+      <span card-footer>
+        <ngx-uswds-button type="button">
+          <span>Visit Florida Keys</span>
+        </ngx-uswds-button>
+      </span>
+    </ngx-uswds-card>
 
-      <li ngx-uswds-card mediaCardType="None" [gridFormat]="['tablet:grid-col-6', 'mobile-lg:grid-col-3']">
-        <span card-header>Card With Media None</span>
-        <span card-media>
-          <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
-            alt="A placeholder image" /></span>
-        <span card-body>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis earum tenetur quo cupiditate, eaque qui
-            officia recusandae.
-          </p>
-        </span>
-        <span card-footer>
-          <ngx-uswds-button type="button">
-            <span>Visit Florida Keys</span>
-          </ngx-uswds-button>
-        </span>
-      </li>
+    <ngx-uswds-card mediaCardType="None">
+      <span card-header>Card with Media</span>
+      <span card-media>
+        <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+          alt="A placeholder" /></span>
+      <span card-body>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          possimus similique nemo odit doloremque laudantium?
+        </p>
+      </span>
+      <span card-footer>
+        <ngx-uswds-button type="button">
+          <span>Visit Florida Keys</span>
+        </ngx-uswds-button>
+      </span>
+    </ngx-uswds-card>
 
-      <li ngx-uswds-card mediaCardType="MediaWHeader">
-        <span card-header>Media with header first</span>
-        <span card-media>
-          <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
-            alt="A placeholder image" /></span>
-        <span card-body>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
-            possimus similique nemo odit doloremque laudantium?
-          </p>
-        </span>
-        <span card-footer>
-          <ngx-uswds-button type="button">
-            <span>Visit Florida Keys</span>
-          </ngx-uswds-button>
-        </span>
-      </li>
+    <ngx-uswds-card mediaCardType="MediaWHeader" [gridFormat]="['tablet:grid-col-12']">
+      <span card-header>Media with Header</span>
+      <span card-media>
+        <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+          alt="A placeholder" /></span>
+      <span card-body>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          possimus similique nemo odit doloremque laudantium?
+        </p>
+      </span>
+      <span card-footer>
+        <ngx-uswds-button type="button">
+          <span>Visit Florida Keys</span>
+        </ngx-uswds-button>
+      </span>
+    </ngx-uswds-card>
 
-      <li ngx-uswds-card mediaCardType="Inset">
-        <span card-header>Inset Media</span>
-        <span card-media>
-          <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
-            alt="A placeholder image" /></span>
-        <span card-body>
-          <p>
-            Etiam vitae sodales metus. Fusce id orci dignissim, efficitur risus eget, tempus odio.
-            Donec lectus ante, auctor eget cursus sed, convallis quis magna. Orci varius natoque penatibus et
-            magnis dis parturient montes, nascetur ridiculus mus. Mauris mattis tellus bibendum aliquet
-            malesuada.
-          </p>
-        </span>
-        <span card-footer>
-          <ngx-uswds-button type="button">
-            <span>Visit Florida Keys</span>
-          </ngx-uswds-button>
-        </span>
-      </li>
+    <ngx-uswds-card mediaCardType="Inset" [gridFormat]="['tablet:grid-col-6']">
+      <span card-header>Inset Card</span>
+      <span card-media>
+        <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+          alt="A placeholder" /></span>
+      <span card-body>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          possimus similique nemo odit doloremque laudantium?
+        </p>
+      </span>
+      <span card-footer>
+        <ngx-uswds-button type="button">
+          <span>Visit Florida Keys</span>
+        </ngx-uswds-button>
+      </span>
+    </ngx-uswds-card>
 
-
-      <li ngx-uswds-card mediaCardType="Exdent">
-        <span card-header>Exdent Media</span>
-        <span card-media>
-          <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
-            alt="A placeholder image" /></span>
-        <span card-body>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Tempore possimus similique nemo odit doloremque laudantium?
-          </p>
-        </span>
-        <span card-footer>
-          <ngx-uswds-button type="button">
-            <span>Visit Florida Keys</span>
-          </ngx-uswds-button>
-        </span>
-      </li>
-
-      <li ngx-uswds-card mediaCardType="FlagDefault">
-        <span card-header>Exdent Media</span>
-        <span card-media>
-          <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
-            alt="A placeholder image" /></span>
-        <span card-body>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Tempore possimus similique nemo odit doloremque laudantium?
-          </p>
-        </span>
-        <span card-footer>
-          <ngx-uswds-button type="button">
-            <span>Visit Florida Keys</span>
-          </ngx-uswds-button>
-        </span>
-      </li>
-
-      <li ngx-uswds-card mediaCardType="FlagRightInset">
-        <span card-header>Exdent Media</span>
-        <span card-media>
-          <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
-            alt="A placeholder image" /></span>
-        <span card-body>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit.
-            Tempore possimus similique nemo odit doloremque laudantium?
-          </p>
-        </span>
-        <span card-footer>
-          <ngx-uswds-button type="button">
-            <span>Visit Florida Keys</span>
-          </ngx-uswds-button>
-        </span>
-      </li>
-
-    </ngx-uswds-card-group>
+    <ngx-uswds-card mediaCardType="Exdent">
+      <span card-header>Exdent Media</span>
+      <span card-media>
+        <img src="https://designsystem.digital.gov/img/introducing-uswds-2-0/built-to-grow--alt.jpg"
+          alt="A placeholder" /></span>
+      <span card-body>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore
+          possimus similique nemo odit doloremque laudantium?
+        </p>
+      </span>
+      <span card-footer>
+        <ngx-uswds-button type="button">
+          <span>Visit Florida Keys</span>
+        </ngx-uswds-button>
+      </span>
+    </ngx-uswds-card>
+  </ngx-uswds-card-group>
 */
 
 @Component({

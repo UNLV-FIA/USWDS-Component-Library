0e97ebcfcc9289b52a9dc0ba27de27d9138ce6a6
charlize-a <charlize.angeles@unlv.edu>
2026-06-24 09:30:21 -0700

    Added main implementation of tag component

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-tag/uswds-tag.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-tag/uswds-tag.ts
@@ -1,21 +1,52 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'ngx-uswds-tag',
-  imports: [],
-  templateUrl: './uswds-tag.html',
-  styleUrl: './uswds-tag.scss',
-})
+import { Component, input } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { TagVariant } from './tag-types';
 
 /***
- * @class
+ * @class UswdsTag
  * @description
  * An Angular standalone component that renders a U.S. Web Design System (USWDS) tag.
  * Tags help draws users' attention to new content, filter results, or show the number of new or unread items for a container.
+ * They support a big visual variant.
+ *
  * @selector ngx-uswds-tag
  *
  * @example
+ * <!-- Using the default tag -->
+ * <ngx-uswds-tag>INFO</ngx-uswds-tag>
+ * <ngx-uswds-tag variant='default'>INFO</ngx-uswds-tag>
  *
- * @input
+ * @example
+ * <!-- Using the big tag -->
+ * <ngx-uswds-tag variant='big'>BIG</ngx-uswds-tag>
+ *
+ * @input {TagVariant} [variant='default'] - Sets the variant style of the tag.
+ *    It's 'default' variant automatically. 'big' sets the tag to big variant.
  */
-export class UswdsTag {}
+@Component({
+  selector: 'ngx-uswds-tag',
+  imports: [NgClass],
+  templateUrl: './uswds-tag.html',
+  styleUrl: './uswds-tag.scss',
+})
+export class UswdsTag {
+  variant = input<TagVariant>('default');
+
+  // Tag variant selection function
+  tagVariantCSS(): string {
+    switch (this.variant()) {
+      case 'default':
+        return '';
+      case 'big':
+        return 'usa-tag--big';
+      default:
+        throw new Error('Invalid variant selected, valid variants are: [default, big]');
+    }
+  }
+}
+
+/*
+TO DO:
+- keep cross checking from uswds-banner
+- check if error comes up
+*/

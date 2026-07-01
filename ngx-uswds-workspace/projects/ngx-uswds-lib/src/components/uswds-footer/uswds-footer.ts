a0e704a58adb5dc1e2aeae19933fe9fceaa17841
charlize-a <charlize.angeles@unlv.edu>
2026-07-01 11:02:24 -0700

    Added file paths for big footer and default rendering if no value is provided for agency/logo

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-footer/uswds-footer.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-footer/uswds-footer.ts
@@ -1,9 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, input, computed } from '@angular/core';
+import { footer } from '@uswds/uswds/js';
 
 @Component({
   selector: 'ngx-uswds-footer',
+  standalone: true,
   imports: [],
   templateUrl: './uswds-footer.html',
   styleUrl: './uswds-footer.scss',
 })
-export class UswdsFooter {}
+export class UswdsFooter {
+  assetsPath = input<string>('/assets/img');
+  agency = input<string>();
+  logoImagePath = input<string>();
+
+  ngOnInit() {
+    footer.on();
+  }
+
+  defaultLogoImagePath = computed(() => `${this.assetsPath()}/logo-img.png`);
+  facebookImagePath = computed(() => `${this.assetsPath()}/usa-icons/facebook.svg`);
+  twitterImagePath = computed(() => `${this.assetsPath()}/usa-icons/twitter.svg`);
+  youtubeImagePath = computed(() => `${this.assetsPath()}/usa-icons/youtube.svg`);
+  instagramImagePath = computed(() => `${this.assetsPath()}/usa-icons/instagram.svg`);
+  rssFeedImagePath = computed(() => `${this.assetsPath()}/usa-icons/rss_feed.svg`);
+}

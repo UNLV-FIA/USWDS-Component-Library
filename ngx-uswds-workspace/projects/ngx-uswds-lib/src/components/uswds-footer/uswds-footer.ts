10df47b20728c08d8e574147bdead6d783c86675
charlize-a <charlize.angeles@unlv.edu>
2026-07-02 09:55:02 -0700

    Added conditional rendering HTML depending on footer variant

--- ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-footer/uswds-footer.ts
+++ ngx-uswds-workspace/projects/ngx-uswds-lib/src/components/uswds-footer/uswds-footer.ts
@@ -1,11 +1,11 @@
 import { Component, input, computed } from '@angular/core';
+import { NgClass } from '@angular/common';
 import { FooterVariant } from './footer-types';
 import { footer } from '@uswds/uswds/js';
 
 @Component({
   selector: 'ngx-uswds-footer',
-  standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './uswds-footer.html',
   styleUrl: './uswds-footer.scss',
 })
@@ -14,10 +14,27 @@ export class UswdsFooter {
   assetsPath = input<string>('/assets/img');
   agency = input<string>();
   logoImagePath = input<string>();
+  // pass buttonStyle, clicked event, need output
 
   ngOnInit() {
     footer.on();
   }
+
+  // Footer variant selection function
+  footerVariantCSS = computed(() => this.footerVariantCSSFn());
+  footerVariantCSSFn = () => {
+    const va = this.variant();
+    switch (va) {
+      case 'big':
+        return 'usa-footer--big';
+      case 'medium':
+        return '';
+      case 'slim':
+        return 'usa-footer--slim';
+      default:
+        throw new Error('Invalid footer variant selected, valid variants are: [big, medium, slim]');
+    } // compile component, check if fixed
+  };
 
   defaultLogoImagePath = computed(() => `${this.assetsPath()}/logo-img.png`);
   facebookImagePath = computed(() => `${this.assetsPath()}/usa-icons/facebook.svg`);
